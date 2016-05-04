3f740e67d632cd849165d297f56d464f8a551eec
rickharris-dev <rickharris724@gmail.com>
2016-05-04 12:34:45 -0700

    Task 109

---
+++ 109_cap_string/1-cap_string.c
@@ -0,0 +1,29 @@
+int is_lowercase(char c) {
+  if (c >= 'a' && c <= 'z')
+    return 1;
+  return 0;
+}
+
+int is_separator(char c) {
+  if (c == ' ' || c == '\t' || c == '\n')
+    return 1;
+  return 0;
+}
+
+char *cap_string(char *s) {
+  int c;
+
+  for (c = 0; s[c] != '\0'; c++){
+    if (c == 0){
+      if(is_lowercase(s[c])) {
+        s[c] = s[c] - 32;
+      }
+    }
+    else if (is_separator(s[c])) {
+      if (is_lowercase(s[c + 1])){
+        s[c + 1] = s[c + 1] - 32;
+      }
+    }
+  }
+  return s;
+}
